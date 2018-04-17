60809ed7c1db9a41a2cbb43eb8ef4192519ad3fc
Vu Thanh Tam <vuthanhtam1506@gmail.com>
2018-04-17 13:57:30 +0700

    Added multi reference example

--- client/js/examples.js
+++ client/js/examples.js
@@ -20,5 +20,6 @@ exports.examples = [
     {title: 'IFML 12', url: 'examples/ifml12.json', image: 'examples/ifml12.png'},
     {title: 'IFML 13', url: 'examples/ifml13.json', image: 'examples/ifml13.png'},
     {title: 'IFML Apple Body Reference Relation', url: 'examples/ifml14.json', image: 'examples/ifml14.png'},
-    {title: 'IFML Apple Body', url: 'examples/ifml15.json', image: 'examples/ifml15.png'}
+    {title: 'IFML Apple Body', url: 'examples/ifml15.json', image: 'examples/ifml15.png'},
+    {title: 'Apple Body - Multi Referance', url: 'examples/ifml16.json', image: 'examples/ifml16.png'}
 ];
